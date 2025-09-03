e8f8b7d7d417aff816e0000baf054ffdfaca283b
NasarImam <imamnasar26@gmail.com>
2025-09-03 11:22:19 +0530

    added first controller class

---
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -0,0 +1,19 @@
+package com.example.todoapispring;
+
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+
+@RestController
+public class TodoController {
+
+    private static List<Todo> todos;
+
+   public TodoController(){
+       todos= new ArrayList<>();
+       todos.add(new Todo(1,false,"Todo 1",1) );
+       todos.add(new Todo(2,true,"Todo 2",2) );
+   }
+
+
+}
